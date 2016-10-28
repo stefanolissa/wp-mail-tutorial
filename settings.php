436b9b2599a46b47b6d936c52e7f8a752aff33ce
Stefano Lissa <stefano.lissa@omnys.com>
2016-10-28 15:53:21 +0200

    Revert "Added the htmlize code"
    This reverts commit 2bea8ef1b2daefa2b6616c4040eb7a12dec02a0b.

--- settings.php
+++ settings.php
@@ -6,16 +6,22 @@
  */
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
+
     $_POST = stripslashes_deep($_POST);
     $to = $_POST['to'];
     $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    wp_mail($to, $subject, $message);
+    $message_text = $_POST['message_text'];
+    $message_html = $_POST['message_html'];
+    if (isset($_POST['btn_text'])) {
+        wp_mail($to, $subject, $message_text);
+    } else if (isset($_POST['btn_html'])) {
+        wp_mail($to, $subject, $message_html, array('Content-Type: text/html'));
+    }
 } else {
     $to = get_option('admin_email');
     $subject = 'Very cool subject';
-    $message = "Hi,\nthis is your first message from WP.\n\nHave a nice day.";
+    $message_text = "Hi,\nthis is your first message from WP.\n\nHave a nice day.";
+    $message_html = "<p>Hi,</p>\n<p>this is strong>your first message</strong> from WP.</p>\n<p>Have a nice day.</p>";
 }
 ?>
 <style>
@@ -29,14 +35,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <div class="wrap">
     <h2>WP Mail Tutorial</h2>
     <form method="post">
-        <input type="email" name="to" value="<?php echo esc_attr($to)?>" placeholder="Email address">
+        <input type="email" name="to" value="<?php echo esc_attr($to) ?>" placeholder="Email address">
         <br>
-        <input type="text" name="subject" value="<?php echo esc_attr($subject)?>" placeholder="Subject">
-        <br>
-        <textarea name="message" placeholder="Message..."><?php echo esc_html($message)?></textarea>
-        <br>
-        <button>Submit</button>
+        <input type="text" name="subject" value="<?php echo esc_attr($subject) ?>" placeholder="Subject">
+
+        <table style="width: 100%">
+            <tr>
+                <td style="width: 50%">
+                    Text version<br>
+                    <textarea name="message_text" placeholder="Message..."><?php echo esc_html($message_text) ?></textarea>
+                    <br>
+                    <button name="btn_text">Send text version</button>
+                </td>
+                <td style="width: 50%">
+                    HTML version<br>
+                    <textarea name="message_html" placeholder="Message..."><?php echo esc_html($message_html) ?></textarea>
+                    <br>
+                    <button name="btn_html">Send HTML version</button>
+                </td>
+            </tr>
+        </table>
+
+
     </form>
-    
+
 </div>
 
