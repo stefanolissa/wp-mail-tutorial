8fc2a19d4edc1d0fcdcdf035531d79d07269ecdb
Stefano Lissa <stefano.lissa@omnys.com>
2016-10-28 11:36:29 +0200

    Docs, comments and so on

--- plugin.php
+++ plugin.php
@@ -9,13 +9,25 @@
   Author URI: https://www.satollo.net/
  */
 
+/**
+ * The class uses "error_log" to write debug information. If in your wp-config.php
+ * you add:
+ * 
+ * define('WP_DEBUG', true);
+ * define('WP_DEBUG_DISPLAY', false);
+ * define('WP_DEBUG_LOG', true);
+ * 
+ * everything is logged in the file wp-content/debug.log. Very useful.
+ */
 class WPMailTutorial {
 
     function __construct() {
 
+        // Here we intercept the hooks needed to change the from address and the from name
         add_filter('wp_mail_from', array($this, 'hook_wp_mail_from'));
         add_filter('wp_mail_from_name', array($this, 'hook_wp_mail_from_name'));
         
+        // Please, admin related code should be executed only in the admin context (be green save energy)
         if (is_admin()) {
             add_action('admin_menu', array($this, 'hook_admin_menu'));
         }
@@ -29,12 +41,15 @@ class WPMailTutorial {
     function hook_wp_mail_from($value) {
         error_log(__METHOD__);
         error_log(print_r($value, true));
+        // We just change the address with a replace, but the address can be saved in an option.
         return str_replace('wordpress@', 'admin@', $value);
     }
 
     function hook_wp_mail_from_name($value) {
         error_log(__METHOD__);
         error_log(print_r($value, true));
+        // The original from name is "WordPress" we change it to the blog name
+        // (the entity decode is required since for emails we need to use the plain blog name)
         return html_entity_decode(get_option('blogname'), ENT_QUOTES, 'UTF-8');
     }
 }
